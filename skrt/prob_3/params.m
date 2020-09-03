869c8b9b7f6f5daf61c1a6ed4deb2e933762ac13
nikhilmadduri <nikmad@users.noreply.github.com>
2020-09-03 22:11:57 +0530

    tyut

--- skrt/prob_3/params.m
+++ skrt/prob_3/params.m
@@ -1,7 +1,6 @@
 
 
 scraft.n =  1.107 * 10^-3; % rad/s
-%  scraft.n = 1000;
 scraft.Jx = 300; % kg-m^2
 scraft.Jy = 400; % kg-m^2
 scraft.Jz = 150; % kg-m^2
@@ -9,7 +8,7 @@ scraft.sim_timestep = 1; % sec
 
 % Prob 3a
 scraft.hx0 =    0;
-scraft.hy0 =    0;
+scraft.hy0 =    -5;
 scraft.hz0 =    0;
 scraft.wx0 =    0;
 scraft.wy0 =    -scraft.n;
--- skrt/prob_3/sdyn.m
+++ skrt/prob_3/sdyn.m
@@ -2,7 +2,7 @@ function yf = sdyn(yi,uu,scraft)
 
 h = 1;
 
-yf = rk4(yi,uu,h,scraft);
+yf = rk4(yi,uu,h,scraft)
 
 end
 
