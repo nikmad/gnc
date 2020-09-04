f0204ad0b158838dbd64668fd11cdb71fb611a2f
nikmad <nikhil.madduri@gmail.com>
2020-09-04 15:33:26 +0530

    fgdf

--- skrt/prob_1b/params.m
+++ skrt/prob_1b/params.m
@@ -10,17 +10,17 @@ swp = 1.3;  % rad/s. structural freq wp
 sdr = 0.002; % structural damping ratio
 
 % Numerator coeffs of CL-TF
-b0 = Kd/swz^2; % S^3 coeff
-b1 = Kp/swz^2 + 2*sdr*Kd/swz; % S^2 coeff
-b2 = 2*sdr*Kp/swz + Kd; % S^1 coeff
-b3 = Kp; % S^0 coeff
+b0 = Kd/swz^2;                  % S^3 coeff
+b1 = Kp/swz^2 + 2*sdr*Kd/swz;   % S^2 coeff
+b2 = 2*sdr*Kp/swz + Kd;         % S^1 coeff
+b3 = Kp;                        % S^0 coeff
 
 % Denominator coeffs of CL-TF
-a0 = J/swp^2; % S^4 coeff
-a1 = 2*sdr*J/swp + Kd/swz^2; % S^3 coeff
-a2 = J + 2*sdr*Kd/swz + Kp/swz^2; % S^2 coeff
-a3 = Kd + 2*sdr*Kp/swz; % S^1 coeff
-a4 = Kp; % S^0 coeff
+a0 = J/swp^2;                       % S^4 coeff
+a1 = 2*sdr*J/swp + Kd/swz^2;        % S^3 coeff
+a2 = J + 2*sdr*Kd/swz + Kp/swz^2;   % S^2 coeff
+a3 = Kd + 2*sdr*Kp/swz;             % S^1 coeff
+a4 = Kp;                            % S^0 coeff
 
 
 
